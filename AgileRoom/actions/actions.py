d70449df444e571b88bedc97db49080a3b7e2eb6
Nicolás Bastien <nbastien@alumnos.exa.unicen.edu.ar>
2022-04-20 19:38:23 -0300

    Cambios en actions

--- AgileRoom/actions/actions.py
+++ AgileRoom/actions/actions.py
@@ -1,36 +1,8 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-
-from actions.event_handling import EventPublisher
-
-import json
-
-url='http://127.0.0.1:5000/dispatcher/send-message'  
-    
-class ActionHelloWorld(Action):
-
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="Hello World!")
-
-         return []
 
 class ActionGuardarNombre(Action):
     def name(self) -> Text:
@@ -46,6 +18,7 @@ class ActionGuardarNombre(Action):
         
         return [SlotSet("nombre",text)]      
 
+# Desformatea el tamaño de un room y envía el mensaje para que sea utilizado en Unity
 class ActionGuardarTamanio(Action):
     def name(self) -> Text:
         return "action_guardar_tamanio"  
@@ -61,9 +34,13 @@ class ActionGuardarTamanio(Action):
         
         dispatcher.utter_message(text = "Crear room," + tamanio_x + "," + tamanio_z)
         return [SlotSet("tamanio_x", tamanio_x), SlotSet("tamanio_z", tamanio_z)]  
-        
-class ActionGuardarObjeto(Action):
 
+class ActionGuardarObjeto(Action):
+    """ 
+        Obtiene el objeto a crear y la cantidad de los slots. Luego, desformatea las posiciones, 
+        verificando que la cantidad obtenida sea igual al número de objetos a crear. Por último, obtiene las posiciones específicas
+        y emite un mensaje." 
+    """
     def name(self) -> Text:
         return "action_guardar_objeto"  
 
